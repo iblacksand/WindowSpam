5fed0b97d2568ecca86d2b9a750dac0e646d9ccf
John Elizarraras <chess4j@gmail.com>
2017-05-16 13:27:31 -0600

    First Push

--- WindowSpam/MainWindow.xaml.cs
+++ WindowSpam/MainWindow.xaml.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace WindowSpam
 {
     /// <summary>
@@ -20,9 +12,59 @@ namespace WindowSpam
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        List<Window> gameWindows = new List<Window>();
         public MainWindow()
         {
             InitializeComponent();
+            timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            timer.Interval = new TimeSpan(0, 5, 0);
+            timer.Start();
         }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e) { }
+
+        private void spawnWindows()
+        {
+            int windowCount = Int16.Parse(WindowCounter.Text);
+            for(int i = 0; i < windowCount; i++)
+            {
+                Window x = RandomWindow();
+                x.Show();
+                gameWindows.Add(x);
+            }
+        }
+
+        private Window RandomWindow() {
+            Random rand = new Random();
+            int win = rand.Next(0, 2);
+            switch (win)
+            {
+                case 0:
+                    return new CutWire();
+                    break;
+                case 1:
+                    return new MakeSandwich();
+                    break;
+                default:
+                    return new CutWire();
+                    break;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            spawnWindows();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+        }
+
     }
 }
